ef89fbbf7ca618e1ffc7df6c12edb840f3aa7efa
Nilo Neregato <neregato.nilo@gmail.com>
2020-06-02 23:39:57 +0200

    convert to web component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './global/index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('plans-root')
-);
+class Plans extends HTMLElement {
+  constructor() {
+    super();
+    this.update();
+  }
+
+  update() {
+    let shadow = this.attachShadow({ mode: 'open'});
+    ReactDOM.render(<App />, shadow);
+  }
+}
+
+window.customElements.define('microfrontend-plans', Plans);
